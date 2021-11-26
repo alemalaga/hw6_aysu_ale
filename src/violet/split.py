2b0cfa8811017696a2322d053cb18404c3074ee8
whatcanaysu <41306207+whatcanaysu@users.noreply.github.com>
2021-11-26 13:53:30 +0100

    split last version

--- src/violet/split.py
+++ src/violet/split.py
@@ -1,14 +1,15 @@
-import hw5
-from hw5 import read_data, split
-
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 class SplitClass:
     def __init__(self, data_name):
         self.data_name = data_name
     
     def train_test(self, prop_test):
-        all_data = read_data(self.data_name)
-        return split(all_data, prop_test)
+        all_data = pd.read_csv(self.data_name)
+        return train_test_split(all_data, test_size = prop_test)
+    
+
 
 # name = "../../diabetes/sample_diabetes_mellitus_data.csv"
 # example = SplitClass(name)
